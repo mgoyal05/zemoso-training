a182a0d8ba96bc8f6cfbe2d75188b2575368cae4
Manish Goyal <mgoyal05.mg@gmail.com>
2017-09-22 18:43:17 +0530

    Hotel Booking

--- SQL/SQL Query Assignments - Pt 1/SQL Query Assignments - Pt 1.5.sql
+++
@@ -1,5 +0,0 @@
-SELECT actor.first_name, actor.last_name,COUNT(film.film_id) AS total_no_of_movies
-FROM film
-INNER JOIN film_actor ON film.film_id = film_actor.film_id
-INNER JOIN actor ON film_actor.actor_id = actor.actor_id
-WHERE actor.first_name = 'SEAN' AND actor.last_name = 'WILLIAMS';
--- SQL/SQL Query Assignments - Pt 2/SQL Query Assignments - Pt 2.1.sql
+++
@@ -1,3 +0,0 @@
-SELECT COUNT(film.film_id) AS number_of_documentaries
-FROM film
-WHERE film.description LIKE '%Documentary%' AND film.special_features LIKE '%Deleted Scenes%';
